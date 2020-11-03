945bf16728b4eafe792e0129f7eb82837dce0dff
camille colet <camcolet@gmail.com>
2020-11-03 12:11:58 +0100

    add completed to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_03_110548) do
+ActiveRecord::Schema.define(version: 2020_11_03_110829) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "completed", default: false
   end
 
 end
